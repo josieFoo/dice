48987cadb1c9631013608404444c312f24dafd09
josiepark <josiepark@mi.fu-berlin.de>
2022-08-30 17:29:06 +0200

    first commit

---
+++ dice_rechner.py
@@ -0,0 +1,54 @@
+import random
+import time
+
+def key_gen(n):
+    """
+    create an integer between 1 to n
+    Args:
+        n : an interger number
+    Returns:
+        _type_: an interger number
+    """
+    
+    return random.randint(1,n)
+
+def reg_gen(n):
+    """
+    create an dictionary
+    Args:
+        n: an interger number
+    length: n
+    keys: 1 to n
+    initial values: 0
+    """
+    
+    reg={}
+    for i in range(1,n+1):
+        reg[i]=0
+    
+    return reg
+
+def dic(n, m):
+    """
+    calculates the probability of keys
+    Args:
+        n (int): number of keys
+        m (int): how many times schould be generated?
+    """
+    
+    reg=reg_gen(n)
+    for i in range (0, m):
+        k=key_gen(n)
+        if k in reg:
+            reg[k]+=1
+        print(reg)
+        
+    for j in range(1, n+1):
+        print(f"{j}: {reg[j]/m*100} %")
+    #print (f"{reg[1]/m*100}%, {reg[2]/m*100}%, {reg[3]/m*100}%, {reg[4]/m*100}%, {reg[5]/m*100}%,{reg[6]/m*100}%")
+    print("expected average: "+ f"{1/n*100}" + " %")
+    #print("tracking"*100)
+#test case
+dic(3,2**12)
+
+
---
+++ dicminmax.py
@@ -0,0 +1,26 @@
+def keywithmaxval(d):
+    """ a) create a list of the dict's keys and values;
+    b) return the key with the max value"""
+    
+    v = list(d.values())
+    k = list(d.keys())
+    max_v = max(v)
+    min_v = min(v)
+    print(v)
+    print(k)
+    #print(k[v.index(max(v))]) 
+    #print(max(d, key = lambda k : d.get(k)))
+    #print(type(v))
+    max_k = []
+    min_k = []
+    for item in d:
+        if max_v == d[item]:
+            max_k.append(item)
+        elif min_v == d[item]:
+            min_k.append(item)
+    print(max_k)
+    print(min_k)
+
+#test
+d={'a': 1, 'b': 2, 'c': 0, 'd': 2, 'e':0}
+keywithmaxval(d)
